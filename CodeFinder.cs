04dd4c9f058281f3ec6be4a2b5aa498d77344e5b
fabianfroding <fabian.froding@gmail.com>
2020-05-15 19:19:06 +0200

    tried adding folder browser dialog to wpf

---
+++ CodeFinder.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+using System.IO;
+
+namespace CodeAnalyzer
+{
+    class CodeFinder
+    {
+        public void FindClasses(string dirPath)
+        {
+            DirectoryInfo di = new DirectoryInfo(dirPath);
+            FileInfo[] files = di.GetFiles();
+
+            foreach (FileInfo fi in files)
+            {
+                if (fi.Name.EndsWith(".cs"))
+                {
+                    Debug.WriteLine(fi.Name);
+                }
+            }
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,6 +8,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -20,14 +22,18 @@ namespace CodeAnalyzer
     /// </summary>
     public partial class MainWindow : Window
     {
+        private static CodeFinder CODE_FINDER;
+
         public MainWindow()
         {
             InitializeComponent();
+            CODE_FINDER = new CodeFinder();
         }
 
         private void BTN_Analyze_Click(object sender, RoutedEventArgs e)
         {
-
+            FolderBrowserDialog fff = new FolderBrowserDialog();
+            
         }
     }
 }
