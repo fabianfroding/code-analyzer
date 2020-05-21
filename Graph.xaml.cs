1223a4c1290642ddcaad56b7490d138d729c824c
fabianfroding <fabian.froding@gmail.com>
2020-05-21 23:56:44 +0200

    fixed bar graph scaling and series title

--- Graph.xaml.cs
+++ Graph.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Controls.DataVisualization.Charting;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace CodeAnalyzer
 {
@@ -28,23 +17,12 @@ namespace CodeAnalyzer
         public Graph(List<KeyValuePair<string, int>> values)
         {
             InitializeComponent();
-            DrawHistogram(values);
+            DrawBarGraph(values);
         }
 
-        public void DrawHistogram(List<KeyValuePair<string, int>> values)
+        public void DrawBarGraph(List<KeyValuePair<string, int>> values)
         {
             ((BarSeries)chart.Series[0]).ItemsSource = values.ToArray();
-
-            /*
-            ((BarSeries)chart.Series[0]).ItemsSource =
-                new KeyValuePair<string, int>[]{
-                new KeyValuePair<string,int>("Information Holder", 12),
-                new KeyValuePair<string,int>("Structurer", 25),
-                new KeyValuePair<string,int>("Service Provider", 5),
-                new KeyValuePair<string,int>("Controller", 6),
-                new KeyValuePair<string,int>("Coordinator", 10),
-                new KeyValuePair<string,int>("Interfacer", 4) };
-                */
         }
     }
 }
