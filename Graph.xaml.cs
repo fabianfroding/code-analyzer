c02e5fc15dd516864e8e5ae4b7441195845a2637
fabianfroding <fabian.froding@gmail.com>
2020-05-23 22:38:28 +0200

    implement scatter series for LOC - associations

--- Graph.xaml.cs
+++ Graph.xaml.cs
@@ -19,10 +19,11 @@ namespace CodeAnalyzer
         public Graph(List<CSharpClass> cSharpClasses)
         {
             InitializeComponent();
-            DrawBarGraph(cSharpClasses);
+            //DrawBarGraph(cSharpClasses);
+            DrawScatterPlot(cSharpClasses);
         }
 
-        public void DrawBarGraph(List<CSharpClass> cSharpClasses)
+        /*public void DrawBarGraph(List<CSharpClass> cSharpClasses)
         {
             var list = new List<KeyValuePair<string, int>>();
 
@@ -33,6 +34,19 @@ namespace CodeAnalyzer
 
             list = list.OrderBy(x => x.Value).ToList();
             ((BarSeries)chart.Series[0]).ItemsSource = list.ToArray();
+        }*/
+
+        public void DrawScatterPlot(List<CSharpClass> cSharpClasses)
+        {
+            var list = new List<KeyValuePair<int, int>>();
+
+            foreach (CSharpClass cSharpClass in cSharpClasses)
+            {
+                list.Add(new KeyValuePair<int, int>(cSharpClass.GetLOC(), cSharpClass.FindAssociationsAmongCSharpClasses(ClassFinder.CSharpClasses).Count));
+            }
+
+            //list = list.OrderBy(x => x.Value).ToList();
+            ((ScatterSeries)scatterChart.Series[0]).ItemsSource = list.ToArray();
         }
     }
 }
