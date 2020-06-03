8bfca98fda23b521013e51fb599c5d4c21ce2391
fabianfroding <fabian.froding@gmail.com>
2020-06-03 14:06:01 +0200

    merged scatter plot into main window

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,11 +15,17 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using CodeAnalyzer.Repositories;
+using LiveCharts;
+using CodeAnalyzer.Controllers;
+using LiveCharts.Configurations;
+using LiveCharts.Wpf;
 
 namespace CodeAnalyzer
 {
     public partial class MainWindow : Window
     {
+        public ChartValues<CSClass> CSClasses { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
@@ -42,17 +48,60 @@ namespace CodeAnalyzer
             if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
                 CSClassRepository.GetCSFilesInDirectory(fbd.SelectedPath);
-
-                // Replace with grah in main window
-                Window window = new Window
-                {
-                    Title = "Scatters",
-                    Content = new ScatterNew()
-                };
-
-                window.ShowDialog();
+                PlotData();
             }
             
         }
+
+        private void PlotData()
+        {
+            CSClasses = new ChartValues<CSClass>();
+
+            foreach (CSClass _CSClass in CSClassController.GetAllCSClasses())
+            {
+                _CSClass.GetAssociationsInListOfCSClasses(CSClassController.GetAllCSClasses());
+                _CSClass.CountLOC();
+                CSClasses.Add(_CSClass);
+            }
+
+            // Force y-axis to have interval based on 1.
+            ScatterPlot1.AxisY.Clear();
+            ScatterPlot1.AxisY.Add(new Axis
+            {
+                Title = "Associations",
+                Separator = new LiveCharts.Wpf.Separator
+                {
+                    Step = 1,
+                    IsEnabled = false // Does what? Maybe prevents separator step to automatically change when resizing window?
+                }
+            });
+
+            ScatterPlot1.DataClick += ChartOnDataClick;
+
+
+            //let create a mapper so LiveCharts know how to plot our CustomerViewModel class
+            var _CSClassVm = Mappers.Xy<CSClass>()
+                .X(value => value.NumLOC) // lets use the position of the item as X
+                .Y(value => value.NumAssociations); //and PurchasedItems property as Y
+
+            //lets save the mapper globally
+            Charting.For<CSClass>(_CSClassVm);
+
+            DataContext = this;
+        }
+
+        private void ChartOnDataClick(object sender, ChartPoint p)
+        {
+            CSClass _CSClass = CSClassController.GetCSClassByIndex(p.Key);
+
+            Window window = new Window
+            {
+                Title = _CSClass.Name,
+                Content = new CSClassView(_CSClass)
+            };
+
+            window.ShowDialog();
+        }
+
     }
 }
