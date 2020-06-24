d7388fdab1e9dd46886efaa11a4d2b8d322712c9
fabianfroding <fabian.froding@gmail.com>
2020-06-24 19:01:42 +0200

    get loaded project data into force directed graph

--- D3WebDocumentWriter.cs
+++ D3WebDocumentWriter.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using CodeAnalyzer.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Text;
 
@@ -29,8 +32,18 @@ namespace CodeAnalyzer
 
                 for (int i = 0; i < FILE_NAMES.Length; i++)
                 {
-                    b = new UTF8Encoding(true).GetBytes(GetFileContent(FILE_NAMES[i]));
-                    fs.Write(b, 0, b.Length);
+                    if (i == 6)
+                    {
+                        Debug.WriteLine(CreateHtmlJSONDataContent());
+                        b = new UTF8Encoding(true).GetBytes(CreateHtmlJSONDataContent());
+                        fs.Write(b, 0, b.Length);
+                    }
+                    else
+                    {
+                        b = new UTF8Encoding(true).GetBytes(GetFileContent(FILE_NAMES[i]));
+                        fs.Write(b, 0, b.Length);
+                    }
+                    
                 }
             }
         }
@@ -40,10 +53,57 @@ namespace CodeAnalyzer
             return new StreamReader(PATH + fileName).ReadToEnd();
         }
 
-        private static string CreateScriptJSONDataContent()
+        private static string CreateHtmlJSONDataContent()
         {
+            List<CSClass> csClasses = CSClassController.GetAllCSClasses(false, false);
 
-            return "";
+            string htmlContent = 
+                "<script>\n" +
+                "var tempdata = {\n" +
+                "\"nodes\": [\n";
+
+            // Generate JSON data for each class
+            for (int i = 0; i < csClasses.Count; i++)
+            {
+                htmlContent += "{ \"id\": \"" + csClasses[i].Name + "\", \"group\": 1 }";
+                if (i == csClasses.Count - 1)
+                {
+                    htmlContent += "\n";
+                }
+                else
+                {
+                    htmlContent += ",\n";
+                }
+            }
+
+            htmlContent += 
+                "],\n" + 
+                "\"links\": [\n";
+
+            // Generate JSON data for each association in each class
+            for (int i = 0; i < csClasses.Count; i++)
+            {
+                List<CSClass> associations = csClasses[i].GetAssociationsInListOfCSClasses(csClasses);
+                for (int j = 0; j < associations.Count; j++)
+                {
+                    htmlContent += "{\"source\": \"" + csClasses[i].Name + "\", \"target\": \"" + associations[j].Name + "\", \"value\": 1 }";
+                    if (i == csClasses.Count - 1 &&  j == associations.Count)
+                    {
+                        htmlContent += "\n";
+                    }
+                    else
+                    {
+                        htmlContent += ",\n";
+                    }
+                }
+            }
+
+            htmlContent +=
+                "]\n" +
+                "};\n" +
+                "</script>";
+
+            return htmlContent;
         }
     }
 }
